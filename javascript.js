b7ce593a4d4133f3c120b25e5dbaf6b6b222222a
TrungMai <bieuluu53@gmail.com>
2024-11-20 09:48:31 -0500

    prompt user to type in

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
+//find random between 0 and 2
 function getComputerChoice(){
     return Math.floor(Math.random() * 3);
 }
 
 console.log(getComputerChoice());
+
+
+//prompt user to type in
+console.log(prompt("Enter your choice: "));
