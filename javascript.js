6454ce99938e2219b200f210eaaf8ad03b3cdd5e
TrungMai <bieuluu53@gmail.com>
2024-11-20 10:04:04 -0500

    add func getComputer and getHuman

--- javascript.js
+++ javascript.js
@@ -4,11 +4,36 @@ function getComputerChoice(){
 }
 
 console.log(getComputerChoice());
-
-
-//prompt user to type in
-console.log(prompt("Enter your choice: "));
-
 //keep track of score
 let humanScore = 0;
 let computerScore = 0;
+
+//get computer choice
+function getComputer(){
+    let x = getComputerChoice();
+    if (x == 0){
+        return "Rock";
+    }
+    else if (x == 1){
+        return "Paper";
+    }
+    else{
+        return "Scissors";
+    }
+}
+
+//get human choice
+function getHuman(){
+    //prompt user to type in
+    return prompt("Enter your choice: ");
+}
+
+//write logic to play a single round
+function playRound(humanChoice, computerChoice){
+    
+}
+
+const humanSelection = getHuman();
+const computerSelection = getComputer();
+
+playRound(humanSelection, computerSelection);
