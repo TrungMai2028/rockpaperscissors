dbc1cd6b7644d610bb3ecb8c3819757e2ec95804
TrungMai <bieuluu53@gmail.com>
2024-11-20 17:50:33 -0500

    upadate playRound

--- javascript.js
+++ javascript.js
@@ -3,7 +3,6 @@ function getComputerChoice(){
     return Math.floor(Math.random() * 3);
 }
 
-console.log(getComputerChoice());
 //keep track of score
 let humanScore = 0;
 let computerScore = 0;
@@ -11,10 +10,10 @@ let computerScore = 0;
 //get computer choice
 function getComputer(){
     let x = getComputerChoice();
-    if (x == 0){
+    if (x === 0){
         return "Rock";
     }
-    else if (x == 1){
+    else if (x === 1){
         return "Paper";
     }
     else{
@@ -30,10 +29,24 @@ function getHuman(){
 
 //write logic to play a single round
 function playRound(humanChoice, computerChoice){
-    
+    if (humanChoice.toLowerCase() == computerChoice.toLowerCase()){
+        console.log("Draw");
+    }
+    else if (humanChoice.toLowerCase() == "rock" && computerChoice.toLowerCase() == "scissors" ||
+    humanChoice.toLowerCase() == "paper" && computerChoice.toLowerCase() == "rock" || 
+    humanChoice.toLowerCase() == "scissors" && computerChoice.toLowerCase() == "paper")
+    {
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+    }
+    else{
+        console.log("You lose! " + computerChoice + " beats " + humanChoice);
+    }
 }
 
 const humanSelection = getHuman();
 const computerSelection = getComputer();
+
+console.log(humanSelection);
+console.log(computerSelection);
 
 playRound(humanSelection, computerSelection);
